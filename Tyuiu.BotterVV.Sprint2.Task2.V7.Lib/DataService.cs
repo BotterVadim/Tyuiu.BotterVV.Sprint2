373173776c3214004e994ab9c54e3991bbaff03b
Tyuiu.BotterVV@outlook.com <masya2012a@gmail.com>
2023-10-14 16:05:25 +0500

    Task2

---
+++ Tyuiu.BotterVV.Sprint2.Task2.V7.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BotterVV.Sprint2.Task2.V7.Lib
+{
+    public class DataService : ISprint2Task2V7
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if (((x < 3 || x > 13) || (y < 3 || y > 13)) || ((x == 6 || x == 7 || x == 8) && (y == 3 || y == 4)) || ((x == 3) && (y == 8 || y == 9 || y == 10 || y == 12 || y == 13)) || ((y == 9 || y == 10 || y == 11) && (x == 6 || x == 7 || x == 8 || x == 9)) || ((y == 12) && (x == 7 || x == 8 || x == 9 || x == 11 || x == 12 || x == 13)) || ((y == 13) && (x == 7 || x == 8 || x == 9 || x == 10 || x == 11 || x == 12 || x == 13)) || ((x == 13) && (y == 3 || y == 4 || y == 5 || y == 9 || y == 10 || y == 11 || y == 12 || y == 13)))
+            {
+                res = false;
+            }
+            else
+            {
+                res = true;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BotterVV.Sprint2.Task2.V7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BotterVV.Sprint2.Task2.V7.Lib;
+
+namespace Tyuiu.BotterVV.Sprint2.Task2.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x, y;
+
+            Console.Title = "Спринт #2 | Выполнил: Боттер В.В. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if                                                       *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Боттер Вадим Валерьевич | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает целые значения с клавиатуры     *");
+            Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y                       *");
+            Console.WriteLine("* в заштрихованной области.                                               *");
+            Console.WriteLine("*                                                                         *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y = ");
+            y = Convert.ToInt32(Console.ReadLine());
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области.");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
