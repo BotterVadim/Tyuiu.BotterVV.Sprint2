a6803c3b1c63454b870057d150d2b3ef6813fb44
Tyuiu.BotterVV@outlook.com <masya2012a@gmail.com>
2023-10-15 10:02:49 +0500

    Task3

---
+++ Tyuiu.BotterVV.Sprint2.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BotterVV.Sprint2.Task3.V7.Lib;
+
+namespace Tyuiu.BotterVV.Sprint2.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds1 = new DataService();
+            double x = 2;
+            double res = ds1.Calculate(x);
+            double wait = 50;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds2 = new DataService();
+            double x = 0;
+            double res = ds2.Calculate(x);
+            double wait = 16.773;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds3 = new DataService();
+            double x = 1;
+            double res = ds3.Calculate(x);
+            double wait = 64;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds4 = new DataService();
+            double x = -12;
+            double res = ds4.Calculate(x);
+            double wait = 20736.25;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BotterVV.Sprint2.Task3.V7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BotterVV.Sprint2.Task3.V7.Lib;
+
+
+namespace Tyuiu.BotterVV.Sprint2.Task3.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            double x;
+
+            Console.Title = "Спринт #2 | Выполнил: Боттер В.В. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if-else                                       *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Боттер Вадим Валерьевич | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой;                                              *"); 
+            Console.WriteLine("*                                                                         *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x));
+
+            Console.ReadKey();
+        }
+    }
+}
