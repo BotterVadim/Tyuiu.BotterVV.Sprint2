5ff557f420a801651cf4c7ad0aa6bc3140af187b
Tyuiu.BotterVV@outlook.com <masya2012a@gmail.com>
2023-10-15 11:09:06 +0500

    Task4

---
+++ Tyuiu.BotterVV.Sprint2.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BotterVV.Sprint2.Task4.V30.Lib;
+
+namespace Tyuiu.BotterVV.Sprint2.Task4.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds1 = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds1.Calculate(x, y);
+            double wait = 8038.679;
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds2 = new DataService();
+            double x = 2;
+            double y = 5;
+            double res = ds2.Calculate(x, y);
+            double wait = 5.5;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BotterVV.Sprint2.Task4.V30/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BotterVV.Sprint2.Task4.V30.Lib;
+
+namespace Tyuiu.BotterVV.Sprint2.Task4.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            double x, y;
+
+            Console.Title = "Спринт #2 | Выполнил: Боттер В.В. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Боттер Вадим Валерьевич | ПКТб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+            Console.WriteLine("* с использованием тернарного оператора, где пользователь вводит          *");
+            Console.WriteLine("* значение переменных X, Y с клавиатуры. Округлить полученное значение до *");
+            Console.WriteLine("* трех знаков после запятой                                               *");
+            Console.WriteLine("*                                                                         *");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y = ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
